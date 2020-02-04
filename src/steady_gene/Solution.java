12069fe166d9bef4b7d0c8e7de65fef9d09dada0
pperalta@afip.gob.ar <pperalta@afip.gob.ar>
2020-02-04 00:54:20 -0300

    Steady Gene. Falta velocidad

--- src/steady_gene/Solution.java
+++ src/steady_gene/Solution.java
@@ -8,67 +8,80 @@ public class Solution {
     // Complete the steadyGene function below.
     static int steadyGene(String gene) {
         Map<Character, Integer> ocurrences = ocurrencesMap(gene);
-
-        Map<Character, Integer[][]> matrixes = new HashMap<>();
-
+        Map<Character, Integer> counters = new HashMap<>();
         Set<Character> posibleLetters = posibleLettersSet(gene);
+        int geneLength = gene.length();
 
         for (Character key : ocurrences.keySet()) {
-            System.out.println("La letra [" + key + "] debe aparecer [" + (ocurrences.get(key) - (gene.length() / 4)) + "] veces");
+            System.out.println("La letra [" + key + "] debe aparecer [" + (ocurrences.get(key) - (geneLength / 4)) + "] veces");
         }
 
-        if(ocurrences.values().stream().allMatch(x -> x - (gene.length() /4) == 0)){
+        if (ocurrences.values().stream().allMatch(x -> x - (geneLength / 4) == 0)) {
             return 0;
         }
 
-        for (Character letter : posibleLetters) {
-            matrixes.put(letter, new Integer[gene.length()][gene.length()]);
-        }
-
-        int minLength = Integer.MAX_VALUE;
-
-        for (int i = 0; i < gene.length(); i++) {
-            for (int j = i; j < gene.length(); j++) {
-                boolean isCandidateSubstring = true;
-
-                System.out.println("[" + i + "][" + j + "] Evaluando: " + gene.substring(i, j + 1));
-
-                for (Character letter : posibleLetters) {
-                    Integer[][] matrix = matrixes.get(letter);
-                    matrix[i][j] = 0;
-
-                    if (j == i) {
-                        if (gene.charAt(i) == letter) {
-                            matrix[i][j] = 1;
-                        }
-                    } else {
-                        if (letter == gene.charAt(j)) {
-                            matrix[i][j] = matrix[i][j - 1] + 1;
-                        } else {
-                            matrix[i][j] = matrix[i][j - 1];
-                        }
-                    }
-
-                    int minimunTimes = ocurrences.get(letter) - (gene.length() / 4);
-
-                    System.out.println("[" + i + "][" + j + "] - Tiene [" + matrix[i][j] + "] letras [" + letter + "], necesita [" + minimunTimes + "]");
-                    if (matrix[i][j] < minimunTimes) {
-                        isCandidateSubstring = false;
-                    }
-                }
-
-                if (isCandidateSubstring) {
-                    System.out.println("[" + i + "][" + j + "] Se encontro ------------------------------------------------------> " + gene.substring(i, j + 1));
-                    if (((j + 1) - i) < minLength) {
-                        System.out.println("[" + i + "][" + j + "] Es la menor hasta ahora !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " + gene.substring(i, j + 1));
-                        minLength = ((j + 1) - i);
-                    }
-                }
-
+        int minTeoricalLength = 0;
+        for(Integer ocurr : ocurrences.values()){
+            if(ocurr - (geneLength / 4) > 0){
+                minTeoricalLength += ocurr - (geneLength / 4);
             }
         }
 
-        return minLength;
+        int minFoundLength = Integer.MAX_VALUE;
+
+        for (int i = 0; i < geneLength; i++) {
+            initMatrixes(counters, posibleLetters);
+            for (int j = i; j < geneLength; j++) {
+
+                int currentStringLength = (j + 1) - i;
+
+                if (currentStringLength < minFoundLength) {
+                    boolean isCandidateSubstring = true;
+
+                    System.out.println("[" + i + "][" + j + "] Evaluando: " + gene.substring(i, j + 1));
+
+                    for (Character letter : posibleLetters) {
+                        int minimunTimes = ocurrences.get(letter) - (geneLength / 4);
+                        Integer counter = counters.get(letter);
+                        char charAtJ = gene.charAt(j);
+
+                        if (letter == charAtJ) {
+                            if (j == i) {
+                                counter = 1;
+                                counters.put(letter, counter);
+                            } else {
+                                counter += 1;
+                                counters.put(letter, counter);
+                            }
+                        }
+
+
+                        System.out.println("[" + i + "][" + j + "] - Tiene [" + counter + "] letras [" + letter + "], necesita [" + minimunTimes + "]");
+                        if (counter < minimunTimes) {
+                            isCandidateSubstring = false;
+                        }
+                    }
+
+                    if (isCandidateSubstring) {
+                        System.out.println("[" + i + "][" + j + "] Se encontro ------------------------------------------------------> " + gene.substring(i, j + 1));
+                        System.out.println("[" + i + "][" + j + "] Es la menor hasta ahora !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " + gene.substring(i, j + 1));
+                        minFoundLength = currentStringLength;
+
+                        if(minFoundLength == minTeoricalLength){
+                            return minFoundLength;
+                        }
+                    }
+                }
+            }
+        }
+
+        return minFoundLength;
+    }
+
+    private static void initMatrixes(Map<Character, Integer> counters, Set<Character> posibleLetters) {
+        for (Character letter : posibleLetters) {
+            counters.put(letter, 0);
+        }
     }
 
     private static Set<Character> posibleLettersSet(String gene) {
