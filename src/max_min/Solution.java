f2bae768c79b3cd65c776dee598477e6fea09cce
pperalta@afip.gob.ar <pperalta@afip.gob.ar>
2020-03-21 21:25:57 -0300

    Max min

---
+++ src/max_min/Solution.java
@@ -0,0 +1,63 @@
+package max_min;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+
+    // Complete the maxMin function below.
+    static int maxMin(int k, int[] arr) {
+        Arrays.sort(arr);
+
+        int minUnfairness = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length - (k-1); i++) {
+            int unfairness = arr[i + (k - 1)] - arr[i];
+
+            if (unfairness < minUnfairness) {
+                minUnfairness = unfairness;
+            }
+        }
+
+        return minUnfairness;
+    }
+
+    private static final Scanner scanner = new Scanner(getSource());
+
+    private static InputStream getSource() {
+        try {
+            return new FileInputStream(".\\src\\max_min\\input16.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return System.in;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(".\\src\\max_min\\output.txt"));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int k = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            arr[i] = arrItem;
+        }
+
+        int result = maxMin(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
