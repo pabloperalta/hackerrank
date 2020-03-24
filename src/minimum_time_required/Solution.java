f9b0b94c4a76391c5d0c9df136743b35ab0cf129
pperalta@afip.gob.ar <pperalta@afip.gob.ar>
2020-03-24 16:55:11 -0300

    Used binary search to reduce the amount of calculations in large data sets

--- src/minimum_time_required/Solution.java
+++ src/minimum_time_required/Solution.java
@@ -7,43 +7,61 @@ public class Solution {
 
     // Complete the minTime function below.
     static long minTime(long[] machines, long goal) {
-
-        long minimumDays = minimumDays(machines, goal);
-        long days = minimumDays;
-
-        int production = 0;
-        while (production < goal) {
-            production = 0;
-
-            for (int i = 0; i < machines.length; i++) {
-                production += days / machines[i];
-            }
-
-            days++;
-        }
-
-        System.out.println("Iterations [" + (days - minimumDays) + "]");
-
-        return days - 1;
-
-    }
-
-    private static long minimumDays(long[] machines, long goal) {
+        long min = Long.MAX_VALUE;
         Double productionPerDay = 0d;
 
-        for (int i = 0; i < machines.length; i++) {
-            productionPerDay += 1d / machines[i];
+        for (long machine : machines) {
+            productionPerDay += 1d / machine;
+
+            if (machine < min) {
+                min = machine;
+            }
         }
 
-        System.out.println("Goal [" + goal + "] production per day [" + productionPerDay + "] minimum days [" + Double.valueOf(goal / productionPerDay).longValue() + "]");
-        return Double.valueOf(goal / productionPerDay).longValue();
+        return binarySearch(machines, machines.length, goal, Double.valueOf(goal / productionPerDay).longValue(), goal * (machines.length / min));
+    }
+
+    // Binary search to find minimum time required to produce M items.
+    static long binarySearch(long[] arr, int arrLength, long goal, long minimumDays, long maxDays) {
+        System.out.println("Min [" + minimumDays + "] Max [" + maxDays + "]");
+        // Doing binary search to find minimum time.
+        while (minimumDays < maxDays) {
+            // Finding the middle value.
+            long mid = (minimumDays + maxDays) / 2L;
+            System.out.println("Min [" + minimumDays + "] Max [" + maxDays + "] Mid[" + mid + "]");
+
+            // Calculate number of items to be produce in mid days.
+            long item = findItems(arr, arrLength, mid);
+            System.out.println("In [" + mid + "] days [" + item + "] items would be produced and the goal is [" + goal + "]");
+
+            if (item < goal) {
+                // If items produce is less than required, set minimumDays = mid + 1.
+                minimumDays = mid + 1;
+            } else {
+                // Else set maxDays = mid.
+                maxDays = mid;
+            }
+        }
+
+        return maxDays;
+    }
+
+    // Return the number of items can be produced in temp sec.
+    static long findItems(long[] arr, long n, long temp) {
+        long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans += (temp / arr[i]);
+        }
+
+        return ans;
     }
 
     private static final Scanner scanner = new Scanner(getSource());
 
     private static InputStream getSource() {
         try {
-            return new FileInputStream(".\\src\\minimum_time_required\\input100.txt");
+            return new FileInputStream(".\\src\\minimum_time_required\\input10.txt");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             return System.in;
